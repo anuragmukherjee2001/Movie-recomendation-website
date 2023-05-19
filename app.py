84cbca4202e46519e0fbdfc0b00d495e732d9884
Anurag Mukherjee <62845847+anuragmukherjee2001@users.noreply.github.com>
2023-05-19 13:33:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-import pickle
+import pickle5 as pickle
 import pandas as pd
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.feature_extraction.text import TfidfVectorizer
