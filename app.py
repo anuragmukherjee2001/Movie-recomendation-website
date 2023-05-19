d0621ee4bc71c1c7f2dbfe9720608eba35ed588f
Anurag Mukherjee <62845847+anuragmukherjee2001@users.noreply.github.com>
2023-05-20 02:48:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,10 +4,36 @@ from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.feature_extraction.text import TfidfVectorizer
 import requests
 
+
 def fetch_poster(movie_id):
     response = requests.get("https://api.themoviedb.org/3/movie/{}?api_key=6faf5353af28d059faa9faeb938867d7&language=en-US".format(movie_id))
     data = response.json()
     return "https://image.tmdb.org/t/p/original/" + data['poster_path']
+
+def fetch_desc(movie_id):
+    response = requests.get("https://api.themoviedb.org/3/movie/{}?api_key=6faf5353af28d059faa9faeb938867d7&language=en-US".format(movie_id))
+    data = response.json()
+    return data['overview']
+
+def release_date(movie_id):
+    response = requests.get("https://api.themoviedb.org/3/movie/{}?api_key=6faf5353af28d059faa9faeb938867d7&language=en-US".format(movie_id))
+    data = response.json()
+    return data['release_date']
+
+def fetch_homepage(movie_id):
+    response = requests.get("https://api.themoviedb.org/3/movie/{}?api_key=6faf5353af28d059faa9faeb938867d7&language=en-US".format(movie_id))
+    data = response.json()
+    return data['homepage']
+
+def genre(movie_id):
+    response = requests.get("https://api.themoviedb.org/3/movie/{}?api_key=6faf5353af28d059faa9faeb938867d7&language=en-US".format(movie_id))
+    data = response.json()
+    gen = data['genres']
+    res = []
+    for i in range(0,len(gen)):
+        res.append(gen[i]['name'])
+
+    return res    
 
 df2 = pd.read_pickle(open('movies.pkl', 'rb'))
 movies = df2['title'].values
@@ -24,20 +50,15 @@ cosine_sim = cosine_similarity(tfidf_matrix, tfidf_matrix)
 
 
 def improved_recommendations(title):
-    # Getting The index of the Input Flim
     idx = indices[title]
 
-    # Getting the Cosine Similarity Tuple of the selected input flim
     sim_scores = list(enumerate(cosine_sim[idx]))
 
-    # Sorting the Similarity Scores
     sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
 
-    # Taking The Top 25 Similairty Scores & movie indices
     sim_scores = sim_scores[1:26]
     movie_indices = [i[0] for i in sim_scores]
 
-    # Getting The Title, Vote Count, Vote Average
     movies = df2.iloc[movie_indices][['id','title', 'vote_count', 'vote_average']]
     vote_counts = movies[movies['vote_count'].notnull()
                          ]['vote_count'].astype('int')
@@ -49,16 +70,13 @@ def improved_recommendations(title):
     def weighted_rating(x):
         v = x['vote_count']
         R = x['vote_average']
-        # Calculation based on the IMDB formula
         return (v/(v+m) * R) + (m/(m+v) * C)
 
-    # Filtering the Movies Vote Count > 0,60 Quantile, Greater than average rating
     qualified = movies[(movies['vote_count'] >= m) & (
         movies['vote_count'].notnull()) & (movies['vote_average'].notnull())]
     qualified['vote_count'] = qualified['vote_count'].astype('int')
     qualified['vote_average'] = qualified['vote_average'].astype('int')
 
-    # Getting the IMDB Weighted Score
     qualified['weighted_score'] = qualified.apply(weighted_rating, axis=1)
     qualified = qualified.sort_values(
         'weighted_score', ascending=False).head(10)
@@ -77,6 +95,10 @@ if st.button('Recommend'):
     cnt = 0
     pred_movie = []
     image_path = []
+    desc = []
+    genres = []
+    released_date = []
+    homepage = []
     for i in df['title']:
         if(cnt != 5):
             col = st.columns(1)
@@ -86,21 +108,30 @@ if st.button('Recommend'):
 
     for i in df['id']:
         image_path.append(fetch_poster(i))
-    
+        desc.append(fetch_desc(i))
+        genres.append(genre(i))
+        released_date.append(release_date(i))
+        homepage.append(fetch_homepage(i))
+       
 
-    col1,col2,col3,col4, col5 = st.columns(5)
-    with col1:
-        st.text(pred_movie[0])
-        st.image(image_path[0])
-    with col2:
-        st.text(pred_movie[1])
-        st.image(image_path[1])
-    with col3:
-        st.text(pred_movie[2])
-        st.image(image_path[2])      
-    with col4:
-        st.text(pred_movie[3])
-        st.image(image_path[3]) 
-    with col5:
-        st.text(pred_movie[4])
-        st.image(image_path[4])    
+    for i in range(0,5):
+        st.subheader(pred_movie[i])
+        col1,col2 = st.columns(2)  
+        
+        with col1:
+            
+            st.image(image_path[i])
+        with col2:
+            st.write(desc[i])
+            st.subheader("Genres")
+            for i in range(0, len(genres[i])):
+                st.write(genres[i][0])
+            st.subheader("Release Date")    
+            st.write(released_date[i]) 
+        if homepage[i] is not '':
+            st.subheader("Get the Movie")
+            st.markdown(f'''
+<a href={homepage[i]}><button style="background-color:GreenYellow;">Click Here</button></a>
+''',
+unsafe_allow_html=True)
+        st.divider()      
